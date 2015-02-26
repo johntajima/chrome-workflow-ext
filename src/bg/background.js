572c07b4bb06e7898bc3547005e0cc934409353b
John Tajima <john@shopify.com>
2015-02-26 18:36:17 -0500

    clean up bg file

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,64 +5,7 @@ chrome.extension.onMessage.addListener(
     var action = request.action;
     var data   = request.data;
     console.log("got request: ", klass, action, data);
-    var reply = Workflow[klass][action](data);
-    console.log("sending reply", reply);
-    sendResponse({data: reply});
+    sendResponse({data: 'done'});
   });
 
 
-
-// ------------------------------------------------------------
-// workflow obj
-// ------------------------------------------------------------
-
-var Workflow = Workflow || {};
-
-// DB that stores the URLs
-Workflow.urls = {
-  db: localStorage,
-
-  // pops next item or returns nil if empty
-  next: function(){
-    var list = JSON.parse(this.db.getItem('workflow:urls'));
-    var url = list.shift();
-    this.set(list);
-    return url;
-  },
-
-  // updates db with text
-  set: function(array){
-    if (!(array instanceof Array)) {
-      array = array.split(/\n|,/);
-      array = _.map(array, function(el){
-        return el.trim();
-      });
-      array = _.reject(array, function(el){
-        return el.length === 0;
-      });
-    }
-
-    console.log("Updating db with ", array);
-    this.db.setItem('workflow:urls', JSON.stringify(array));
-    return array.join("\n");
-  },
-
-  list: function(){
-    if (this.db['workflow:urls']) {
-      return JSON.parse(this.db.getItem('workflow:urls'));
-    } else {
-      return null;
-    }
-  },
-  // empty db
-  clear: function(){
-    this.db.setItem('workflow:urls', null);
-  }
-};
-
-
-// DB that stores the template/form json
-Workflow.templates = {
-  get: function(key){},
-  set: function(key, value){}
-};
