e3af94bcbcc3cbe731b4326355166ed963958157
John Tajima <john@shopify.com>
2015-02-26 22:22:59 -0500

    form data stored in localstorage as array of key/value pairs

--- src/bg/background.js
+++ src/bg/background.js
@@ -18,7 +18,11 @@ $(document).ready(function(){
             Workitem.next(tab.id);
             break;
           case "workitem:get_form":
-            data = Workitem.build_form();
+            data = Workitem.buildForm();
+            break;
+          case "workitem:save":
+            data = 'error';
+            Workitem.saveData(request.data);
             break;
         }
 
@@ -31,20 +35,61 @@ $(document).ready(function(){
 
 
 var Workitem = Workitem || {};
+
+Workitem.current_url = function() {
+  return localStorage.getItem('workflow:current_url');
+};
+
 Workitem.next = function(tab_id) {
   var url = Workflow.urls.next();
   console.log("[bg] Opening next url ", url);
   Workflow.tabs.open(url);
 };
 Workitem.done = function() {
-  var url = localStorage.getItem('workflow:current_url');
+  var url = this.current_url();
   if (Workflow.urls.next() === url) {
     Workflow.urls.pop();
   }
 };
 
-Workitem.build_form = function() {
-  return '<div id="workform">hello</div>';
+
+
+//
+// save array of [{key: <>, value: <>}] of data
+// into format of
+// [{key: 'url', value: 'http://'}, {key: 'asdf', value: 'asdf'}, ...]
+Workitem.saveData = function(data) {
+  var record = [{key: 'url', value: this.current_url()}];
+  console.log("ercord is ", record);
+  record = record.concat(data);
+  console.log("Saving data ", record);
+
+  var records = JSON.parse(localStorage.getItem("workflow:data")) || [];
+  records.push(record);
+
+  var entry = JSON.stringify(records);
+
+  localStorage.setItem("workflow:data", entry);
 };
+
+Workitem.buildForm = function() {
+  var text  = "\
+<div id='workitem-frame'>\
+  <h4>Workflow Form</h4>\
+  <form class='form form-vertical' id='workitem-form'>\
+    <div class='form-group'>\
+      <label>Tag</label>\
+      <input type='text' name='tag' class='form-control'>\
+    </div>\
+  </form>\
+  <button id='workitem-save-btn' class='btn btn-primary'>Save Data</button>\
+  <button id='workitem-next-btn' class='btn btn-success pull-right'>Next >></button>\
+  <div id='workform-msg'></div>\
+</div>\
+  ";
+
+  return text;
+};
+
 
 
--- src/inject/workitem.js
+++ src/inject/workitem.js
@@ -4,6 +4,13 @@
 // render form template
 // listen to submit/next button
 
+$(document).ready(function(){
+  // init form listeners
+  $(document).on('click', '#workitem-next-btn', WorkItem.next);
+  $(document).on('click', '#workitem-save-btn', WorkItem.saveData);
+
+});
+
 var WorkItem = WorkItem || {};
 chrome.runtime.sendMessage({action: "workitem:start"}, function(response) {
   if (response.data === 'ok') {
@@ -25,6 +32,17 @@ WorkItem.next = function() {
   chrome.runtime.sendMessage({action: "workitem:next"}, function(response) {
     console.log(response.data);
   });
+};
 
+WorkItem.saveData = function(){
+  var data = $('#workitem-form').serializeArray();
+  chrome.runtime.sendMessage({action: "workitem:save", data: data}, function(response) {
+    console.log(response.data);
+    if (response.data === 'ok') {
+      WorkItem.next();
+    } else {
+      $("#workform-msg").html(response.data);
+    }
+  });
 };
 
